3f5dd2ba1155ddcf7739ce8ae34a90393eb4765a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/DocumentServerDemoUtils.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using DevExpress.Spreadsheet;
+
+public static class StreamCopyHelper {
+    public static void Copy(Stream src, Stream dst) {
+        const int bufferSize = 32768;
+        byte[] buffer = new byte[bufferSize];
+        int bytesRead = 0;
+        do {
+            bytesRead = src.Read(buffer, 0, bufferSize);
+            dst.Write(buffer, 0, bytesRead);
+
+        }
+        while(bytesRead == bufferSize);
+    }
+}
+
+public class HtmlContentGenerator {
+    Stream dstStream;
+    public HtmlContentGenerator(Stream dstStream) {
+        this.dstStream = dstStream;
+    }
+    public void Generate(IWorkbook book, int sheetIndx) {
+        DevExpress.XtraSpreadsheet.Model.DocumentModel model = book.Model;
+        MemoryStream tempStream = new MemoryStream();
+        DevExpress.XtraSpreadsheet.Export.HtmlDocumentExporterOptions options = new DevExpress.XtraSpreadsheet.Export.HtmlDocumentExporterOptions();
+        options.SheetIndex = sheetIndx;
+        options.EmbedImages = true;
+        model.InternalAPI.SaveDocumentHtmlContent(tempStream, options);
+        tempStream.Seek(0, SeekOrigin.Begin);
+        StreamCopyHelper.Copy(tempStream, this.dstStream);
+    }
+    public void Generate(IWorkbook book) {
+        Generate(book, 0);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to display an XLSX document on the webpage
+
+
+<p>This example illustrates how to load a document into a <a href="http://documentation.devexpress.com/#DocumentServer/clsDevExpressSpreadsheetWorkbooktopic"><u>Workbook Class</u></a> instance and display its sheets on the webpage. Note that this approach is also used in our <a href="http://demos.devexpress.com/ASPxDocumentServerDemos/SpreadSheet/ExpenseReport.aspx"><u>demos</u></a>.<br><br>The<strong> Universal Subscription</strong> or an additional <strong>Document Server Subscription</strong> is required to use this example in production code. Please refer to the <a href="http://www.devexpress.com/Subscriptions/">DevExpress Subscription</a> page for pricing information.</p>
+
+<br/>
+
+
